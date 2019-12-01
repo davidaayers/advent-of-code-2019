2f47f55660bbffb18fc4c1d04dbcd5bbdb4840de
David Ayers <davidaayers@gmail.com>
2019-12-01 00:21:33 -0700

    Part 1 solution
    Took way too long to get here, mostly because I was futzing around with
    VSCode. Having a heck of a time with the linter messing up my code,
    finally gave up and switched to sublime text.
    
    Also got bit by /r/n line endings instead of /n.
    
    But, finally got the right answer. Hurray!

--- day01/day01.go
+++ day01/day01.go
@@ -1,19 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	//"math"
+	"strconv"
+	"strings"
+)
 
-// Part1 Part 1 of puzzle
+// Part1 Part 1 of uzzle
 func Part1(input string) string {
-	return "Answer"
+ 	var totalFuel = 0
+
+	for _, s := range strings.Split(input, "\r\n") {
+		w, _ := strconv.Atoi(s) 
+		totalFuel += CalcFuel(w)
+	}
+
+	return "Answer " + strconv.Itoa(totalFuel)
 }
 
-// Part2 Part 2 of puzzle
+// CalcFuel Calculate the fuel equired for oe module
+func CalcFuel(weight int) int {
+	return weight / 3 - 2
+}
+
+// Part2 Part2 of puzzle
 func Part2(input string) string {
 	return "Answer"
 }
 
 func main() {
-	input := ""
-	fmt.Println("Part 1: " + Part1(input))
-	fmt.Println("Part 2: " + Part2(input))
+	bytes, _ := ioutil.ReadFile("input.txt")
+
+	fmt.Println("Part 1: " + Part1(string(bytes)))
+	//fmt.Println("Part 2: " + Part2(string(bytes)))
 }
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -2,6 +2,25 @@ package main
 
 import "testing"
 
+var testCases = []struct {
+	moduleWeight int
+	expectedFuel int
+}{
+	{12, 2},
+	{14, 2},
+	{1969, 654},
+	{100756, 33583},
+}
+
+func TestCalcFuel(t *testing.T) {
+	for _, testCase := range testCases {
+		result := CalcFuel(testCase.moduleWeight)
+		if result != testCase.expectedFuel {
+			t.Errorf("Error, expected %d fuel, got %d", testCase.expectedFuel, result)
+		}
+	}
+}
+
 func TestPart1(t *testing.T) {
 	answer := Part1("test")
 	if answer != "Answer" {
