322606c1967d46ffd1165587fc27c3394eb98b2e
David Ayers <davidaayers@gmail.com>
2019-12-02 22:37:17 -0700

    Test & working code for initial intcode runner
    Now, to parse the puzzle input and run it for realz, and
    see what lies in store for part2!

--- day02/day02.go
+++ day02/day02.go
@@ -3,10 +3,27 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"strconv"
-	"strings"
+	//"strconv"
+	//"strings"
 )
 
+
+func RunIntCode(code []int) {
+	idx := 0
+	for {
+		opCode := code[idx]
+
+		if opCode == 99 {
+			break
+		} else if opCode == 1 {
+			code[code[idx+3]] = code[code[idx+1]] + code[code[idx+2]]
+		} else if opCode == 2 {
+			code[code[idx+3]] = code[code[idx+1]] * code[code[idx+2]]
+		}
+
+		idx += 4
+	}
+}
 
 // Part1 Part 1 of puzzle
 func Part1(input string) string {
--- day02/day02_test.go
+++ day02/day02_test.go
@@ -5,7 +5,54 @@ import (
 	"testing"
 )
 
-// go test -timeout 30s day01 -run '^(TestCalcFuelIncludingWeightOfFuel)$'
+// go test -timeout 30s day01 -run '^(TestRunIntCode)$'
+var testCases = []struct {
+	startingCode []int 
+	endingCode []int
+} {
+	{
+		[]int{1,9,10,3,2,3,11,0,99,30,40,50},
+		[]int{3500,9,10,70,2,3,11,0,99,30,40,50},
+	},
+	{
+		[]int{1,0,0,0,99}, 
+		[]int{2,0,0,0,99},
+	},
+	{
+		[]int{2,3,0,3,99}, 
+		[]int{2,3,0,6,99},
+	},
+	{
+		[]int{2,4,4,5,99,0}, 
+		[]int{2,4,4,5,99,9801},
+	},
+	{
+		[]int{1,1,1,4,99,5,6,0,99}, 
+		[]int{30,1,1,4,2,5,6,0,99},
+	},
+}
+
+func Equal(a, b []int) bool {
+    if len(a) != len(b) {
+        return false
+    }
+    for i, v := range a {
+        if v != b[i] {
+            return false
+        }
+    }
+    return true
+}
+
+func TestRunIntCode(t *testing.T) {
+	
+	for _, testCase := range testCases {
+		RunIntCode(testCase.startingCode)
+		if !Equal(testCase.startingCode,testCase.endingCode) {
+			t.Errorf("Error, expected %v got %v", testCase.endingCode, testCase.startingCode)	
+		}
+	}
+}
 
 func TestPart1(t *testing.T) {
 	bytes, _ := ioutil.ReadFile("input.txt")
