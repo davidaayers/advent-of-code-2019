ee7be7a197ed60dcb22e22ce34b1566cfd121a6c
David Ayers <davidaayers@gmail.com>
2019-12-03 23:37:18 -0700

    Answer to part 2
    Went down some dead ends on this one, but overall happy with where it
    ended up. Not sure if this is good go code or not.

--- day04/day04.go
+++ day04/day04.go
@@ -2,19 +2,113 @@ package main
 
 import (
 	"fmt"
-	//"sort"
-	//"strconv"
-	//"strings"
+	"strconv"
 )
+
+func IsValidPassword(password int) bool {
+	passStr := strconv.Itoa(password)
+
+	// It is a six-digit number.
+	if len(passStr) != 6 {
+		return false
+	}
+
+	foundAdjacent := false
+	for i := 1; i < len(passStr); i++ {
+		d1,_ := strconv.Atoi(string(passStr[i-1]))
+		d2,_ := strconv.Atoi(string(passStr[i]))
+
+		// Two adjacent digits are the same (like `22` in `122345`).
+		if d1 == d2 {
+			foundAdjacent = true
+		}
+
+		// Going from left to right, the digits never decrease; they only ever increase or stay the same (like `111123` or `135679`).
+		if d1 > d2 {
+			return false
+		}
+	}
+
+	if !foundAdjacent {
+		return false
+	}
+
+	return true
+}
+
+func IsMoreValidPassword(password int) bool {
+	passStr := strconv.Itoa(password)
+
+	// It is a six-digit number.
+	if len(passStr) != 6 {
+		return false
+	}
+
+	for i := 1; i < len(passStr); i++ {
+		d1,_ := strconv.Atoi(string(passStr[i-1]))
+		d2,_ := strconv.Atoi(string(passStr[i]))
+
+		// Going from left to right, the digits never decrease; they only ever increase or stay the same (like `111123` or `135679`).
+		if d1 > d2 {
+			return false
+		}
+	}
+
+	// break it into chunks based on repeating numbers
+	chunks := make([]string,0)
+	chunk := string(passStr[0])
+
+	for i := 1; i < len(passStr); i++ {
+		if passStr[i] == passStr[i-1] {
+			chunk += string(passStr[i])
+		} else {
+			chunks = append(chunks,chunk)
+			chunk = string(passStr[i])
+		}
+	}
+
+	// append last chunk
+	chunks = append(chunks,chunk)
+
+	// one of the chunks must be 2
+	foundDouble := false
+	for _,ch := range(chunks) {
+		if len(ch) == 2 {
+			foundDouble = true
+		}
+	}
+
+	if !foundDouble {
+		return false
+	}
+
+	return true
+}
 
 // Part1 Part 1 of puzzle
 func Part1(start, end int) string {
-	return "Answer: "
+	validPasswords := 0
+
+	for i := start; i < end + 1 ; i ++ {
+		if IsValidPassword(i) {
+			validPasswords++
+		}
+	}
+
+	return "Answer: " + strconv.Itoa(validPasswords)
 }
 
 // Part2 Part2 of puzzle
 func Part2(start, end int) string {
-	return "Answer: "
+	validPasswords := 0
+
+	for i := start; i < end + 1 ; i ++ {
+		if IsMoreValidPassword(i) {
+			validPasswords++
+		}
+	}
+
+	return "Answer: " + strconv.Itoa(validPasswords)
 }
 
 func main() {
--- day04/day04_test.go
+++ day04/day04_test.go
@@ -4,20 +4,58 @@ import (
 	"testing"
 )
 
-// go test -timeout 30s day03 -run '^(TestPart1)$'
+var testCases = []struct {
+	password int
+	valid bool
+} {
+	{ 111111, true  },
+	{ 223450, false },
+	{ 123789, false },
+}
+
+var testCases2 = []struct {
+	password int
+	valid bool
+} {
+	{ 112233, true  },
+	{ 123444, false },
+	{ 111122, true },
+}
+
+// go test -timeout 30s day04 -run '^(TestIsValidPassword)$'
+func TestIsValidPassword(t *testing.T) {
+	for _, testCase := range testCases {
+		isValid := IsValidPassword(testCase.password)
+		if isValid != testCase.valid {
+			t.Errorf("For %v, expected %v got %v", testCase.password, testCase.valid, isValid)	
+		}
+	}
+}
+
+// go test -timeout 30s day04 -run '^(TestIsMoreValidPassword)$'
+func TestIsMoreValidPassword(t *testing.T) {
+	for _, testCase := range testCases2 {
+		isValid := IsMoreValidPassword(testCase.password)
+		if isValid != testCase.valid {
+			t.Errorf("For %v, expected %v got %v", testCase.password, testCase.valid, isValid)	
+		}
+	}
+}
+
+// go test -timeout 30s day04 -run '^(TestPart1)$'
 func TestPart1(t *testing.T) {
 	start,end := 197487,673251
-	expected := "Answer: "
+	expected := "Answer: 1640"
 	answer := Part1(start,end)
 	if answer != expected {
 		t.Errorf("Error, expected %s got %s", expected, answer)
 	}
 }
 
-// go test -timeout 30s day03 -run '^(TestPart2)$'
+// go test -timeout 30s day04 -run '^(TestPart2)$'
 func TestPart2(t *testing.T) {
 	start,end := 197487,673251
-	expected := "Answer: "
+	expected := "Answer: 1126"
 	answer := Part2(start,end)
 	if answer != expected {
 		t.Errorf("Error, expected %s got %s", expected, answer)
