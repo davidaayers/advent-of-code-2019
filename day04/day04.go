53fa61f3007dee612ca3e0dcbcd48956e40210ea
David Ayers <davidaayers@gmail.com>
2019-12-03 21:57:20 -0700

    Day 04 prep & template creation for future days

---
+++ day04/day04.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	//"sort"
+	//"strconv"
+	//"strings"
+)
+
+// Part1 Part 1 of puzzle
+func Part1(input string) string {
+	return "Answer: "
+}
+
+// Part2 Part2 of puzzle
+func Part2(input string) string {
+	return "Answer: "
+}
+
+func main() {
+	bytes, _ := ioutil.ReadFile("input.txt")
+
+	fmt.Println("Part 1: " + Part1(string(bytes)))
+	fmt.Println("Part 2: " + Part2(string(bytes)))
+}
