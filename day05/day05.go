89c3afa48b1a8b4ff9b314c4e5bc58e75e5c8da8
David Ayers <davidaayers@gmail.com>
2019-12-05 07:32:39 -0700

    Refactored to DRY the code up a bit
    I wasn't satisfied with the amount of repetition, and I saw an example
    of using an anonymous function to extract the params, so I implemented
    that. Code is much nicer to read now.

--- day05/day05.go
+++ day05/day05.go
@@ -25,60 +25,47 @@ func RunIntCode(code []int,input int) []int {
 	for {
 
 		paddedIntCode := fmt.Sprintf("%05d",code[idx])
-		param1PositionMode := paddedIntCode[2:3] == "0"
-		param2PositionMode := paddedIntCode[1:2] == "0"
-
 		opCode, _ := strconv.Atoi(paddedIntCode[3:5])
 
 		if opCode == 99 {
 			return output
 		} 
 		
+		paramModeMap := map[int] bool {
+			1:paddedIntCode[2:3] == "0",
+			2:paddedIntCode[1:2] == "0",
+			3:paddedIntCode[0:1] == "0",
+		}
+
+		getParam := func(pos int)int {
+			param := code[idx+pos]
+			if paramModeMap[pos] {
+				param = code[code[idx+pos]]
+			}
+			return param
+		}
+
 		if opCode == 1 || opCode == 2 {
 			// Opcode `1` adds together numbers read from two positions and stores the result in a third position.
 			// Opcode `2` works exactly like opcode `1`, except it multiplies the two inputs instead of adding them.
-			param1 := code[idx+1]
-			if param1PositionMode {
-				param1 = code[code[idx+1]]
-			}
-
-			param2 := code[idx+2]
-			if param2PositionMode {
-				param2 = code[code[idx+2]]
-			}
-
 			if opCode == 1 {
-				code[code[idx+3]] = param1 + param2	
+				code[code[idx+3]] = getParam(1) + getParam(2)
 			} else if opCode == 2 {
-				code[code[idx+3]] = param1 * param2	
+				code[code[idx+3]] = getParam(1) * getParam(2)
 			}
 		} else if opCode == 3 {
 			// Opcode `3` takes a single integer as input and saves it to the address given by its only parameter.
 			code[code[idx+1]] = input
 		} else if opCode == 4 {
 			// Opcode `4` outputs the value of its only parameter.
-			param1 := code[idx+1]
-			if param1PositionMode {
-				param1 = code[code[idx+1]]
-			}
-			output = append(output,param1)
+			output = append(output, getParam(1))
 		} else if opCode == 5 || opCode == 6 {
 			// Opcode `5` is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the 
 			// value from the second parameter. Otherwise, it does nothing.
 			// Opcode `6` is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the 
 			// value from the second parameter. Otherwise, it does nothing.
-			param1 := code[idx+1]
-			if param1PositionMode {
-				param1 = code[code[idx+1]]
-			}
-
-			param2 := code[idx+2]
-			if param2PositionMode {
-				param2 = code[code[idx+2]]
-			}
-
-			if (opCode == 5 && param1 != 0)||(opCode == 6 && param1 == 0) {
-				idx = param2
+			if (opCode == 5 && getParam(1) != 0)||(opCode == 6 && getParam(1) == 0) {
+				idx = getParam(2)
 			} else {
 				idx += opCodeLengths[opCode]
 			}
@@ -87,17 +74,7 @@ func RunIntCode(code []int,input int) []int {
 			// the position given by the third parameter. Otherwise, it stores 0.
 			// Opcode `8` is equals: if the first parameter is equal to the second parameter, it stores 1 in the 
 			// position given by the third parameter. Otherwise, it stores 0.
-			param1 := code[idx+1]
-			if param1PositionMode {
-				param1 = code[code[idx+1]]
-			}
-
-			param2 := code[idx+2]
-			if param2PositionMode {
-				param2 = code[code[idx+2]]
-			}
-
-			if (opCode == 7 && param1 < param2) || (opCode == 8 && param1 == param2) {
+			if (opCode == 7 && getParam(1) < getParam(2)) || (opCode == 8 && getParam(1) == getParam(2)) {
 				code[code[idx+3]] = 1
 			} else {
 				code[code[idx+3]] = 0
