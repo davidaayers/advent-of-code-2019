6420b7a6e3ff296adbd460eb0c9eeac0a67513a7
David Ayers <davidaayers@gmail.com>
2019-12-04 23:21:30 -0700

    Solution to part 1
    Man I messed this one up a bunch before I got it right! :) No
    test cases were provided so it was tough to do this one TDD style,
    so I resorted to a lot of prints and looking for my mistake. Once
    I found it, the rest was easy

--- day05/day05.go
+++ day05/day05.go
@@ -4,13 +4,88 @@ import (
 	"fmt"
 	"io/ioutil"
 	//"sort"
-	//"strconv"
-	//"strings"
+	"strconv"
+	"strings"
 )
+
+var opCodeLengths = map[int]int{
+	1:4,
+	2:4,
+	3:2,
+	4:2,
+}
+
+// RunIntCode Our intCode interpreter
+func RunIntCode(code []int,input int) {
+	idx := 0
+	//cnt := 0
+	for {
+
+		paddedIntCode := fmt.Sprintf("%05d",code[idx])
+		param1PositionMode := paddedIntCode[2:3] == "0"
+		param2PositionMode := paddedIntCode[1:2] == "0"
+		//param3PositionMode := paddedIntCode[0:1] == "0"
+
+		opCode, _ := strconv.Atoi(paddedIntCode[3:5])
+
+		fmt.Printf("%v > %v (%v): paramMode[1]: %v paramMode[2]: %v ", idx,paddedIntCode,opCode, param1PositionMode, param2PositionMode)
+
+		if opCode == 99 {
+			return
+		} 
+		
+		if opCode == 1 || opCode == 2 {
+			param1 := code[idx+1]
+			if param1PositionMode {
+				param1 = code[code[idx+1]]
+			}
+
+			param2 := code[idx+2]
+			if param2PositionMode {
+				param2 = code[code[idx+2]]
+			}
+
+			if opCode == 1 {
+				code[code[idx+3]] = param1 + param2	
+			} else {
+				code[code[idx+3]] = param1 * param2	
+			}
+
+			fmt.Printf(" Param1: %v Param2: %v Result: %v Stored in: %v\n",param1,param2,code[code[idx+3]],code[idx+3])
+
+		} else if opCode == 3 {
+			code[code[idx+1]] = input
+			fmt.Printf(" Input: %v Stored in: %v\n", input, code[idx+1])
+		} else if opCode == 4 {
+			param1 := code[code[idx+1]]
+			if !param1PositionMode {
+				param1 = code[idx+1]
+			}
+			fmt.Printf("Value is %v\n", param1)
+		}
+
+		idx += opCodeLengths[opCode]
+	}
+}
+
+// ParseIntCode Parses a comma delimted input string into an array of intCode
+func ParseIntCode(input string) []int {
+	strs := strings.Split(strings.ReplaceAll(input,"\r\n",""),",")
+	intCode := make([]int, len(strs))
+	for idx, s := range strs {
+		i, _ := strconv.Atoi(s) 
+		intCode[idx] = i
+	}
+	return intCode
+}
 
 // Part1 Part 1 of puzzle
 func Part1(input string) string {
-	return "Answer: "
+	intCode := ParseIntCode(input)
+
+	RunIntCode(intCode,1)
+
+	return "Answer: " + strconv.Itoa(intCode[0])
 }
 
 // Part2 Part2 of puzzle
