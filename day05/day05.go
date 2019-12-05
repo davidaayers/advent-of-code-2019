8e7ac8acb1c12325a21a3b42df87869b5c24de6b
David Ayers <davidaayers@gmail.com>
2019-12-05 07:41:34 -0700

    Ran go fmt to fix code formatting

--- day05/day05.go
+++ day05/day05.go
@@ -8,36 +8,29 @@ import (
 )
 
 var opCodeLengths = map[int]int{
-	1:4,
-	2:4,
-	3:2,
-	4:2,
-	5:3,
-	6:3,
-	7:4,
-	8:4,
+	1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4,
 }
 
 // RunIntCode Our intCode interpreter
-func RunIntCode(code []int,input int) []int {
-	output := make([]int,0)	
+func RunIntCode(code []int, input int) []int {
+	output := make([]int, 0)
 	idx := 0
 	for {
 
-		paddedIntCode := fmt.Sprintf("%05d",code[idx])
+		paddedIntCode := fmt.Sprintf("%05d", code[idx])
 		opCode, _ := strconv.Atoi(paddedIntCode[3:5])
 
 		if opCode == 99 {
 			return output
-		} 
-		
-		paramModeMap := map[int] bool {
-			1:paddedIntCode[2:3] == "0",
-			2:paddedIntCode[1:2] == "0",
-			3:paddedIntCode[0:1] == "0",
 		}
 
-		getParam := func(pos int)int {
+		paramModeMap := map[int]bool{
+			1: paddedIntCode[2:3] == "0",
+			2: paddedIntCode[1:2] == "0",
+			3: paddedIntCode[0:1] == "0",
+		}
+
+		getParam := func(pos int) int {
 			param := code[idx+pos]
 			if paramModeMap[pos] {
 				param = code[code[idx+pos]]
@@ -60,19 +53,19 @@ func RunIntCode(code []int,input int) []int {
 			// Opcode `4` outputs the value of its only parameter.
 			output = append(output, getParam(1))
 		} else if opCode == 5 || opCode == 6 {
-			// Opcode `5` is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the 
+			// Opcode `5` is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the
 			// value from the second parameter. Otherwise, it does nothing.
-			// Opcode `6` is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the 
+			// Opcode `6` is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the
 			// value from the second parameter. Otherwise, it does nothing.
-			if (opCode == 5 && getParam(1) != 0)||(opCode == 6 && getParam(1) == 0) {
+			if (opCode == 5 && getParam(1) != 0) || (opCode == 6 && getParam(1) == 0) {
 				idx = getParam(2)
 			} else {
 				idx += opCodeLengths[opCode]
 			}
 		} else if opCode == 7 || opCode == 8 {
-			// Opcode `7` is less than: if the first parameter is less than the second parameter, it stores 1 in 
+			// Opcode `7` is less than: if the first parameter is less than the second parameter, it stores 1 in
 			// the position given by the third parameter. Otherwise, it stores 0.
-			// Opcode `8` is equals: if the first parameter is equal to the second parameter, it stores 1 in the 
+			// Opcode `8` is equals: if the first parameter is equal to the second parameter, it stores 1 in the
 			// position given by the third parameter. Otherwise, it stores 0.
 			if (opCode == 7 && getParam(1) < getParam(2)) || (opCode == 8 && getParam(1) == getParam(2)) {
 				code[code[idx+3]] = 1
@@ -93,10 +86,10 @@ func RunIntCode(code []int,input int) []int {
 
 // ParseIntCode Parses a comma delimted input string into an array of intCode
 func ParseIntCode(input string) []int {
-	strs := strings.Split(strings.ReplaceAll(input,"\r\n",""),",")
+	strs := strings.Split(strings.ReplaceAll(input, "\r\n", ""), ",")
 	intCode := make([]int, len(strs))
 	for idx, s := range strs {
-		i, _ := strconv.Atoi(s) 
+		i, _ := strconv.Atoi(s)
 		intCode[idx] = i
 	}
 	return intCode
@@ -106,7 +99,7 @@ func ParseIntCode(input string) []int {
 func Part1(input string) string {
 	intCode := ParseIntCode(input)
 
-	output := RunIntCode(intCode,1)
+	output := RunIntCode(intCode, 1)
 
 	return "Answer: " + strconv.Itoa(output[len(output)-1])
 }
@@ -115,7 +108,7 @@ func Part1(input string) string {
 func Part2(input string) string {
 	intCode := ParseIntCode(input)
 
-	output := RunIntCode(intCode,5)
+	output := RunIntCode(intCode, 5)
 
 	return "Answer: " + strconv.Itoa(output[len(output)-1])
 }
--- day05/day05_test.go
+++ day05/day05_test.go
@@ -6,118 +6,116 @@ import (
 )
 
 var testCases = []struct {
-	startingCode []int 
-	input int
-	expected []int
-} {
+	startingCode []int
+	input        int
+	expected     []int
+}{
 	// Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
 	{
-		[]int{3,9,8,9,10,9,4,9,99,-1,8},
+		[]int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8},
 		8,
 		[]int{1},
 	},
 	{
-		[]int{3,9,8,9,10,9,4,9,99,-1,8},
+		[]int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8},
 		9,
 		[]int{0},
 	},
 	// Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
 	{
-		[]int{3,9,7,9,10,9,4,9,99,-1,8},
+		[]int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8},
 		9,
 		[]int{0},
 	},
 	{
-		[]int{3,9,7,9,10,9,4,9,99,-1,8},
+		[]int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8},
 		3,
 		[]int{1},
 	},
 	// 3,3,1108,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
 	{
-		[]int{3,3,1108,-1,8,3,4,3,99},
+		[]int{3, 3, 1108, -1, 8, 3, 4, 3, 99},
 		8,
 		[]int{1},
 	},
 	{
-		[]int{3,3,1108,-1,8,3,4,3,99},
+		[]int{3, 3, 1108, -1, 8, 3, 4, 3, 99},
 		9,
 		[]int{0},
 	},
 	// 3,3,1107,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
 	{
-		[]int{3,3,1107,-1,8,3,4,3,99},
+		[]int{3, 3, 1107, -1, 8, 3, 4, 3, 99},
 		3,
 		[]int{1},
 	},
 	{
-		[]int{3,3,1107,-1,8,3,4,3,99},
+		[]int{3, 3, 1107, -1, 8, 3, 4, 3, 99},
 		9,
 		[]int{0},
 	},
 	// Here are some jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero:
 	// (using position mode)
 	{
-		[]int{3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9},
+		[]int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9},
 		0,
 		[]int{0},
 	},
 	{
-		[]int{3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9},
+		[]int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9},
 		1,
 		[]int{1},
 	},
 	// (using immediate mode)
 	{
-		[]int{3,3,1105,-1,9,1101,0,0,12,4,12,99,1},
+		[]int{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1},
 		0,
 		[]int{0},
 	},
 	{
-		[]int{3,3,1105,-1,9,1101,0,0,12,4,12,99,1},
+		[]int{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1},
 		1,
 		[]int{1},
 	},
-	// This example program uses an input instruction to ask for a single number. The program will then 
-	// output `999` if the input value is below `8`, output `1000` if the input value is equal to `8`, or 
+	// This example program uses an input instruction to ask for a single number. The program will then
+	// output `999` if the input value is below `8`, output `1000` if the input value is equal to `8`, or
 	// output `1001` if the input value is greater than 8.
-	// 
+	//
 	{
-		[]int{3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99},
+		[]int{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99},
 		1,
 		[]int{999},
 	},
 	{
-		[]int{3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99},
+		[]int{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99},
 		8,
 		[]int{1000},
 	},
 	{
-		[]int{3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99},
+		[]int{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99},
 		9,
 		[]int{1001},
 	},
-
-
 }
 
 func Equal(a, b []int) bool {
-    if len(a) != len(b) {
-        return false
-    }
-    for i, v := range a {
-        if v != b[i] {
-            return false
-        }
-    }
-    return true
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
 }
 
 // go test -timeout 30s day05 -run '^(TestRunIntCode)$'
 func TestRunIntCode(t *testing.T) {
 	for _, testCase := range testCases {
-		output := RunIntCode(testCase.startingCode,testCase.input)
-		if !Equal(output,testCase.expected) {
-			t.Errorf("Error, expected %v got %v", testCase.expected, output)	
+		output := RunIntCode(testCase.startingCode, testCase.input)
+		if !Equal(output, testCase.expected) {
+			t.Errorf("Error, expected %v got %v", testCase.expected, output)
 		}
 	}
 }
