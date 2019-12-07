065a7024d27ea9dd0eb120e955b066ddcd5562e1
David Ayers <davidaayers@gmail.com>
2019-12-06 22:33:04 -0700

    Day 07 prep & test cases

--- day07/day07.go
+++ day07/day07.go
@@ -3,10 +3,109 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"strings"
+
 	//"sort"
 	//"strconv"
 	//"strings"
 )
+
+func CalculateThrust(intCode []int, phaseSequence []int) int {
+
+
+
+
+	return 0
+}
+
+var opCodeLengths = map[int]int{
+	1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4,
+}
+
+// RunIntCode Our intCode interpreter
+func RunIntCode(code []int, input []int) []int {
+	output := make([]int, 0)
+	idx := 0
+	inputIdx := 0
+	for {
+
+		paddedIntCode := fmt.Sprintf("%05d", code[idx])
+		opCode, _ := strconv.Atoi(paddedIntCode[3:5])
+
+		if opCode == 99 {
+			return output
+		}
+
+		paramModeMap := map[int]bool{
+			1: paddedIntCode[2:3] == "0",
+			2: paddedIntCode[1:2] == "0",
+			3: paddedIntCode[0:1] == "0",
+		}
+
+		getParam := func(pos int) int {
+			param := code[idx+pos]
+			if paramModeMap[pos] {
+				param = code[code[idx+pos]]
+			}
+			return param
+		}
+
+		if opCode == 1 || opCode == 2 {
+			// Opcode `1` adds together numbers read from two positions and stores the result in a third position.
+			// Opcode `2` works exactly like opcode `1`, except it multiplies the two inputs instead of adding them.
+			if opCode == 1 {
+				code[code[idx+3]] = getParam(1) + getParam(2)
+			} else if opCode == 2 {
+				code[code[idx+3]] = getParam(1) * getParam(2)
+			}
+		} else if opCode == 3 {
+			// Opcode `3` takes a single integer as input and saves it to the address given by its only parameter.
+			code[code[idx+1]] = input[inputIdx]
+			inputIdx++
+		} else if opCode == 4 {
+			// Opcode `4` outputs the value of its only parameter.
+			output = append(output, getParam(1))
+		} else if opCode == 5 || opCode == 6 {
+			// Opcode `5` is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the
+			// value from the second parameter. Otherwise, it does nothing.
+			// Opcode `6` is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the
+			// value from the second parameter. Otherwise, it does nothing.
+			if (opCode == 5 && getParam(1) != 0) || (opCode == 6 && getParam(1) == 0) {
+				idx = getParam(2)
+			} else {
+				idx += opCodeLengths[opCode]
+			}
+		} else if opCode == 7 || opCode == 8 {
+			// Opcode `7` is less than: if the first parameter is less than the second parameter, it stores 1 in
+			// the position given by the third parameter. Otherwise, it stores 0.
+			// Opcode `8` is equals: if the first parameter is equal to the second parameter, it stores 1 in the
+			// position given by the third parameter. Otherwise, it stores 0.
+			if (opCode == 7 && getParam(1) < getParam(2)) || (opCode == 8 && getParam(1) == getParam(2)) {
+				code[code[idx+3]] = 1
+			} else {
+				code[code[idx+3]] = 0
+			}
+		} else {
+			panic("Unexpected op code")
+		}
+
+		if opCode != 5 && opCode != 6 {
+			idx += opCodeLengths[opCode]
+		}
+	}
+}
+
+// ParseIntCode Parses a comma delimted input string into an array of intCode
+func ParseIntCode(input string) []int {
+	strs := strings.Split(strings.ReplaceAll(input, "\r\n", ""), ",")
+	intCode := make([]int, len(strs))
+	for idx, s := range strs {
+		i, _ := strconv.Atoi(s)
+		intCode[idx] = i
+	}
+	return intCode
+}
 
 // Part1 Part 1 of puzzle
 func Part1(input string) string {
--- day07/day07_test.go
+++ day07/day07_test.go
@@ -5,7 +5,37 @@ import (
 	"testing"
 )
 
-// go test -timeout 30s day03 -run '^(TestPart1)$'
+var testCases = []struct {
+	startingCode   []int
+	phaseSequence  []int
+	expectedThrust int
+}{
+	{
+		[]int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0},
+		[]int{4, 3, 2, 1, 0},
+		43210,
+	},
+	{
+		[]int{3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0},
+		[]int{0, 1, 2, 3, 4},
+		54321,
+	},
+	{
+		[]int{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0},
+		[]int{1, 0, 4, 3, 2},
+		65210,
+	},
+}
+
+func TestRunIntCode(t *testing.T) {
+	for _, testCase := range testCases {
+		thrust := CalculateThrust(testCase.startingCode, testCase.phaseSequence)
+		if thrust != testCase.expectedThrust {
+			t.Errorf("Error, expected %v got %v", testCase.expectedThrust, thrust)
+		}
+	}
+}
+
 func TestPart1(t *testing.T) {
 	bytes, _ := ioutil.ReadFile("input.txt")
 	expected := "Answer: "
@@ -15,7 +45,6 @@ func TestPart1(t *testing.T) {
 	}
 }
 
-// go test -timeout 30s day03 -run '^(TestPart2)$'
 func TestPart2(t *testing.T) {
 	bytes, _ := ioutil.ReadFile("input.txt")
 	expected := "Answer: "
