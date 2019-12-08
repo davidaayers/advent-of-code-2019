8ce6db7e98a49b99e2b759ccc0fee89b4054c252
David Ayers <davidaayers@gmail.com>
2019-12-08 07:37:13 -0700

    Solution to day 8, part 1
    First part was easy, and the way I built it, ends up being
    completly unrelated to how part 2 will be solved :laughing:

--- day08/day08.go
+++ day08/day08.go
@@ -3,14 +3,37 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	//"sort"
-	//"strconv"
-	//"strings"
+	"math"
+	"strconv"
+	"strings"
 )
+
+func sifChecksum(sif string, width int, height int) int {
+	leastZeros := math.MaxInt64
+	leastZeroLayer := map[int]int{}
+
+	for i := 0; i < len(sif); i += width * height {
+		layerStr := sif[i : i+width*height]
+		layer := make(map[int]int)
+		pixels := strings.Split(layerStr, "")
+
+		for _, pixelStr := range pixels {
+			pixel, _ := strconv.Atoi(pixelStr)
+			layer[pixel]++
+		}
+
+		if layer[0] < leastZeros {
+			leastZeros = layer[0]
+			leastZeroLayer = layer
+		}
+	}
+	return leastZeroLayer[1] * leastZeroLayer[2]
+}
 
 // Part1 Part 1 of puzzle
 func Part1(input string) string {
-	return "Answer: "
+	width, height := 25, 6
+	return "Answer: " + strconv.Itoa(sifChecksum(input, width, height))
 }
 
 // Part2 Part2 of puzzle
--- day08/day08_test.go
+++ day08/day08_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestPart1(t *testing.T) {
 	bytes, _ := ioutil.ReadFile("input.txt")
-	expected := "Answer: "
+	expected := "Answer: 1064"
 	answer := Part1(string(bytes))
 	if answer != expected {
 		t.Errorf("Error, expected %s got %s", expected, answer)
