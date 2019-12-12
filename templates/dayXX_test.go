0f213c80029db5a2cff3f4d48d2ebef059a168d3
David Ayers <davidaayers@gmail.com>
2019-12-12 14:43:08 -0700

    Removed comments with command line run details
    Also, I made the tests *fail* by default, instead of *pass* by default.

--- templates/dayXX_test.go
+++ templates/dayXX_test.go
@@ -5,20 +5,18 @@ import (
 	"testing"
 )
 
-// go test -timeout 30s day03 -run '^(TestPart1)$'
 func TestPart1(t *testing.T) {
 	bytes, _ := ioutil.ReadFile("input.txt")
-	expected := "Answer: "
+	expected := "Answer: WRONG"
 	answer := Part1(string(bytes))
 	if answer != expected {
 		t.Errorf("Error, expected %s got %s", expected, answer)
 	}
 }
 
-// go test -timeout 30s day03 -run '^(TestPart2)$'
 func TestPart2(t *testing.T) {
 	bytes, _ := ioutil.ReadFile("input.txt")
-	expected := "Answer: "
+	expected := "Answer: WRONG"
 	answer := Part2(string(bytes))
 	if answer != expected {
 		t.Errorf("Error, expected %s got %s", expected, answer)
