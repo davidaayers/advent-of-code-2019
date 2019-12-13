a9a28778656a90964535a46244fecd9a5fcea061
David Ayers <davidaayers@gmail.com>
2019-12-13 07:53:01 -0700

    Day 13, Part 2 Solution
    Simple breakout-playing AI implementation. Awesome stuff.

--- day13/day13.go
+++ day13/day13.go
@@ -24,15 +24,99 @@ var tiles = map[int]string{
 type ArcadeCabinet struct {
 	memory []int
 	screen Screen
+	score  int
 }
 
-func (arcadeCabinet *ArcadeCabinet) RunGame() {
+func (arcadeCabinet *ArcadeCabinet) RunGame(freePlay, renderScreen bool) {
+	if !freePlay {
+		arcadeCabinet.RunGameRenderScreenOnly(renderScreen)
+		return
+	}
+
+	// free games!
+	arcadeCabinet.memory[0] = 2
+
+	lastInstructionPointer, lastRelativeBase := 0, 0
+	terminated := false
+	var output []int
+	var instr []int
+	var input []int
+	ballX, paddleX := 0, 0
+	var gameStarted = false
+
+	for {
+		output, lastInstructionPointer, lastRelativeBase, terminated =
+			RunIntCode(arcadeCabinet.memory, input, lastInstructionPointer, lastRelativeBase, true)
+		if terminated {
+			break
+		}
+
+		// in freeplay mode, output will be one character at a time, so we need to build up our instructions
+		instr = append(instr, output[0])
+		if len(instr) == 3 {
+			x, y, tileId := instr[0], instr[1], instr[2]
+			if x == -1 {
+				arcadeCabinet.score = tileId
+			} else {
+				arcadeCabinet.screen.AddTile(x, y, tiles[tileId])
+
+				// remember where the ball and paddle are
+				if tileId == 4 {
+					ballX = x
+				}
+
+				if tileId == 3 {
+					paddleX = x
+				}
+			}
+
+			instr = nil
+		}
+
+		if renderScreen {
+			arcadeCabinet.RenderScreen()
+		}
+
+		if !gameStarted && lastInstructionPointer == 73 {
+			gameStarted = true
+		}
+
+		if gameStarted {
+			input = nil
+			// adjust the paddle position to move it toward the ball
+			/*
+				If the joystick is in the neutral position, provide 0.
+				If the joystick is tilted to the left, provide -1.
+				If the joystick is tilted to the right, provide 1.
+			*/
+
+			joystickCommand := 0
+			if ballX > paddleX {
+				joystickCommand = 1
+			} else if ballX < paddleX {
+				joystickCommand = -1
+			}
+			input = append(input, joystickCommand)
+		}
+
+	}
+}
+
+func (arcadeCabinet *ArcadeCabinet) RunGameRenderScreenOnly(renderScreen bool) {
 	output, _, _, _ := RunIntCode(arcadeCabinet.memory, []int{}, 0, 0, false)
 
 	for idx := 0; idx < len(output); idx += 3 {
 		x, y, tileId := output[idx], output[idx+1], output[idx+2]
 		arcadeCabinet.screen.AddTile(x, y, tiles[tileId])
 	}
+	if renderScreen {
+		arcadeCabinet.RenderScreen()
+	}
+}
+
+func (arcadeCabinet ArcadeCabinet) RenderScreen() {
+	fmt.Printf("-------------- Score: %05d ----------------\n", arcadeCabinet.score)
+	arcadeCabinet.screen.RenderScreen()
 }
 
 type Screen struct {
@@ -82,14 +166,16 @@ func NewArcadeCabinet(intCode []int) (arcadeCabinet ArcadeCabinet) {
 func Part1(input string) string {
 	intCode := ParseIntCode(input)
 	arcadeCabinet := NewArcadeCabinet(intCode)
-	arcadeCabinet.RunGame()
-	arcadeCabinet.screen.RenderScreen()
+	arcadeCabinet.RunGame(false, false)
 	return "Answer: " + strconv.Itoa(arcadeCabinet.screen.tileCounts[tiles[2]])
 }
 
 // Part2 Part2 of puzzle
 func Part2(input string) string {
-	return "Answer: "
+	intCode := ParseIntCode(input)
+	arcadeCabinet := NewArcadeCabinet(intCode)
+	arcadeCabinet.RunGame(true, false)
+	return "Answer: " + strconv.Itoa(arcadeCabinet.score)
 }
 
 func main() {
--- day13/day13_test.go
+++ day13/day13_test.go
@@ -8,7 +8,7 @@ import (
 // go test -timeout 30s day03 -run '^(TestPart1)$'
 func TestPart1(t *testing.T) {
 	bytes, _ := ioutil.ReadFile("input.txt")
-	expected := "Answer: "
+	expected := "Answer: 318"
 	answer := Part1(string(bytes))
 	if answer != expected {
 		t.Errorf("Error, expected %s got %s", expected, answer)
@@ -18,7 +18,7 @@ func TestPart1(t *testing.T) {
 // go test -timeout 30s day03 -run '^(TestPart2)$'
 func TestPart2(t *testing.T) {
 	bytes, _ := ioutil.ReadFile("input.txt")
-	expected := "Answer: "
+	expected := "Answer: 16309"
 	answer := Part2(string(bytes))
 	if answer != expected {
 		t.Errorf("Error, expected %s got %s", expected, answer)
