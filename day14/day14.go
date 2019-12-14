650fe39b9fcbbb7ec038df358078eae41419a2e7
David Ayers <davidaayers@gmail.com>
2019-12-13 22:01:55 -0700

    Prep for day 14

---
+++ day14/day14.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+// Part1 Part 1 of puzzle
+func Part1(input string) string {
+	return "Answer: "
+}
+
+// Part2 Part2 of puzzle
+func Part2(input string) string {
+	return "Answer: "
+}
+
+func main() {
+	bytes, _ := ioutil.ReadFile("input.txt")
+
+	fmt.Println("Part 1: " + Part1(string(bytes)))
+	fmt.Println("Part 2: " + Part2(string(bytes)))
+}
