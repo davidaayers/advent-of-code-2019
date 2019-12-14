12ce64701a09f3500c11803a78833ab2a7db7660
David Ayers <davidaayers@gmail.com>
2019-12-13 22:01:21 -0700

    Work on fixing day 10, still not successful

--- day10/day10.go
+++ day10/day10.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
+	"strconv"
 	"strings"
 )
 
@@ -11,120 +13,22 @@ type Universe struct {
 	asteroids   []Asteroid
 }
 
-func getLine(from, to Point) (points []Point) {
-	x1, y1 := from.x, from.y
-	x2, y2 := to.x, to.y
-
-	isSteep := Abs(y2-y1) > Abs(x2-x1)
-	if isSteep {
-		x1, y1 = y1, x1
-		x2, y2 = y2, x2
-	}
-
-	reversed := false
-	if x1 > x2 {
-		x1, x2 = x2, x1
-		y1, y2 = y2, y1
-		reversed = true
-	}
-
-	deltaX := x2 - x1
-	deltaY := Abs(y2 - y1)
-	err := deltaX / 2
-	y := y1
-	var ystep int
-
-	if y1 < y2 {
-		ystep = 1
-	} else {
-		ystep = -1
-	}
-
-	for x := x1; x < x2+1; x++ {
-		if isSteep {
-			points = append(points, Point{y, x})
-		} else {
-			points = append(points, Point{x, y})
-		}
-		err -= deltaY
-		if err < 0 {
-			y += ystep
-			err += deltaX
-		}
-	}
-
-	if reversed {
-		//Reverse the slice
-		for i, j := 0, len(points)-1; i < j; i, j = i+1, j-1 {
-			points[i], points[j] = points[j], points[i]
-		}
-	}
-
-	return
+func Angle(from Asteroid, to Asteroid) float64 {
+	dx := float64(from.x - to.x)
+	dy := float64(from.y - to.y)
+	theta := math.Atan2(dy, dx)
+	theta *= 180 / math.Pi
+	return theta
 }
 
-func (universe Universe) CanObserve(from, to Asteroid) bool {
-	if from == to {
-		return false
-	}
-	line := getLine(from.point, to.point)
-	fmt.Printf("Line from %v to %v: %v ", from, to, line)
-	line = line[1 : len(line)-1]
-	fmt.Printf("Chomped: %v\n", line)
-
-	for _, checkPoint := range line {
-		if universe.universeMap[checkPoint.y][checkPoint.x] == "#" {
-			return false
-		}
+func Angle360(from Asteroid, to Asteroid) float64 {
+	theta := Angle(from, to)
+	if theta < 0 {
+		theta = 360 + theta
 	}
 
-	return true
-}
-
-func (universe Universe) CanObserve2(from, to Asteroid) bool {
-	// use Bresenham's to draw a line from -> to. If we hit
-	// another asteroid on the way, we can't see to's position
-	// implementation below inspired by
-	// http://www.roguebasin.com/index.php?title=Bresenham%27s_Line_Algorithm#JavaScript
-	x1, y1 := from.point.x, from.point.y
-	x2, y2 := to.point.x, to.point.y
-
-	isSteep := Abs(y2-y1) > Abs(x2-x1)
-	if isSteep {
-		x1, y1 = y1, x1
-		x2, y2 = y2, x2
-	}
-
-	deltaX := x2 - x1
-	deltaY := Abs(y2 - y1)
-	err := deltaX / 2
-	y := y1
-	var ystep int
-
-	if y1 < y2 {
-		ystep = 1
-	} else {
-		ystep = -1
-	}
-
-	for x := x1; x < x2+1; x++ {
-		if isSteep {
-			if universe.universeMap[x][y] == "#" {
-				return false
-			}
-		} else {
-			if universe.universeMap[y][x] == "#" {
-				return false
-			}
-		}
-		err -= deltaY
-		if err < 0 {
-			y += ystep
-			err += deltaX
-		}
-	}
-
-	return true
+	theta = math.Round(theta*1000) / 1000
+	return theta
 }
 
 func Abs(x int) int {
@@ -139,7 +43,7 @@ type Point struct {
 }
 
 type Asteroid struct {
-	point                  Point
+	Point
 	numObservableAsteroids int
 }
 
@@ -172,18 +76,31 @@ func PrintMap(universe Universe) {
 }
 
 func FindBestLocation(universe *Universe) Asteroid {
-	highestNumObservableAsteroids := 0
 	var bestLocation Asteroid
+	highestViewableAsteroids := 0
 
 	for i := 0; i < len(universe.asteroids)-1; i++ {
+		angles := make(map[float64]int)
 		for j := 0; j < len(universe.asteroids)-1; j++ {
-			if universe.CanObserve(universe.asteroids[i], universe.asteroids[j]) {
-				universe.asteroids[i].numObservableAsteroids++
-				if universe.asteroids[i].numObservableAsteroids > highestNumObservableAsteroids {
-					highestNumObservableAsteroids = universe.asteroids[i].numObservableAsteroids
-					bestLocation = universe.asteroids[i]
-				}
+			if universe.asteroids[i].Point == universe.asteroids[j].Point {
+				continue
 			}
+			angle := Angle360(universe.asteroids[i], universe.asteroids[j])
+			angles[angle]++
+		}
+		// all of the entries in angles with a count of 1 are asteroids that aren't
+		// occluded by any others
+
+		viewableAsteroids := 0
+		for _, v := range angles {
+			if v == 1 {
+				viewableAsteroids++
+			}
+		}
+		viewableAsteroids = len(angles)
+		universe.asteroids[i].numObservableAsteroids = viewableAsteroids
+		if viewableAsteroids > highestViewableAsteroids {
+			bestLocation = universe.asteroids[i]
 		}
 	}
 	return bestLocation
@@ -191,7 +108,9 @@ func FindBestLocation(universe *Universe) Asteroid {
 
 // Part1 Part 1 of puzzle
 func Part1(input string) string {
-	return "Answer: "
+	universe := ParseMap(input)
+	bestLocation := FindBestLocation(&universe)
+	return "Answer: " + strconv.Itoa(bestLocation.numObservableAsteroids)
 }
 
 // Part2 Part2 of puzzle
