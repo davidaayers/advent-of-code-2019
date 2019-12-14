67ed0a42adf6dd9350ef0d16141bd55c66ecbe19
David Ayers <davidaayers@gmail.com>
2019-12-14 00:02:45 -0700

    WIP commit for day 14
    Struggling with this one :(

--- day14/day14.go
+++ day14/day14.go
@@ -3,7 +3,121 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
 )
+
+type ReactionRecipe struct {
+	Ingredient
+	inputs []Ingredient
+}
+
+func (recipe ReactionRecipe) requiresOre() bool {
+	// must have only 1 input, and that input must be ore
+	if len(recipe.inputs) == 1 && recipe.inputs[0].result == "ORE" {
+		return true
+	}
+	return false
+}
+
+type Ingredient struct {
+	result string
+	count  int
+}
+
+func DetermineRequiredOre(reactions []string) int {
+	recipes := make(map[string]ReactionRecipe, len(reactions))
+	for _, reaction := range reactions {
+		recipe := parseReaction(reaction)
+		recipes[recipe.result] = recipe
+	}
+
+	fmt.Printf("Recipes: %v\n", recipes)
+
+	ingredients := make(map[string]int)
+
+	MakeReaction("FUEL", recipes, ingredients, 1)
+
+	ore := 0
+	for k, v := range ingredients {
+		if recipes[k].requiresOre() {
+			ore += CalcOre(recipes[k], v)
+		}
+		fmt.Printf("ingredient: %v qty: %v\n", k, v)
+	}
+
+	return ore
+}
+
+func CalcOre(recipe ReactionRecipe, numDesired int) int {
+	fmt.Printf("-> Recipe %v NumDesired: %v\n", recipe, numDesired)
+	batches := int(math.Ceil(float64(numDesired) / float64(recipe.count)))
+	oreProduced := batches * recipe.inputs[0].count
+	fmt.Printf("-> %v need %v batches of ore at %v ore per batch: %v\n", recipe.result, batches, recipe.inputs[0].count, oreProduced)
+	return oreProduced
+}
+
+func MakeReaction(desiredElement string, recipes map[string]ReactionRecipe, ingredients map[string]int, numDesired int) {
+	recipe := recipes[desiredElement]
+	//"2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG",
+	// need 53 STKFG, so
+	// 2 * 53 = 106 VPVL
+	// 7 * 53
+	for _, input := range recipe.inputs {
+		if input.result != "ORE" {
+			// how many batches do I need
+			batches := int(math.Ceil(float64(numDesired) / float64(recipe.count)))
+			ingredients[input.result] += batches * input.count
+			MakeReaction(input.result, recipes, ingredients, input.count)
+		}
+	}
+}
+
+func parseReaction(reaction string) ReactionRecipe {
+	parts := strings.Split(reaction, "=>")
+	reactionResult, resultCnt := parseElement(parts[1])
+	//fmt.Printf("%v: %v\n", reactionResult, resultCnt)
+
+	recipe := ReactionRecipe{
+		Ingredient: Ingredient{reactionResult, resultCnt},
+		inputs:     make([]Ingredient, 0),
+	}
+
+	for _, inputStr := range strings.Split(parts[0], ",") {
+		input, inputCnt := parseElement(inputStr)
+		//fmt.Printf("-> %v: %v\n", input, inputCnt)
+		recipe.inputs = append(recipe.inputs, Ingredient{
+			result: input,
+			count:  inputCnt,
+		})
+	}
+
+	//fmt.Printf("Recipe: %v\n", recipe)
+	return recipe
+}
+
+func MakeReaction2(desiredElement string, recipes map[string]ReactionRecipe, cnt int) int {
+	//recipe := recipes[desiredElement]
+	//
+	//ore := 0
+	//for _, input := range recipe.inputs {
+	//	if input.result == "ORE" {
+	//		ore += input.count * cnt
+	//	} else {
+	//		ore += MakeReaction(input.result, recipes, input.count)
+	//	}
+	//}
+	return 0
+}
+
+func parseElement(element string) (elementName string, count int) {
+	element = strings.TrimSpace(element)
+	parts := strings.Split(element, " ")
+	elementName = parts[1]
+	count, _ = strconv.Atoi(parts[0])
+	return
+}
 
 // Part1 Part 1 of puzzle
 func Part1(input string) string {
--- day14/day14_test.go
+++ day14/day14_test.go
@@ -5,6 +5,85 @@ import (
 	"testing"
 )
 
+/*
+10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL
+
+*/
+
+var testCases = []struct {
+	reactions   []string
+	expectedOre int
+}{
+	{
+		[]string{
+			"10 ORE => 10 A",
+			"1 ORE => 1 B",
+			"7 A, 1 B => 1 C",
+			"7 A, 1 C => 1 D",
+			"7 A, 1 D => 1 E",
+			"7 A, 1 E => 1 FUEL",
+		},
+		31,
+	},
+	{
+		[]string{
+			"9 ORE => 2 A",
+			"8 ORE => 3 B",
+			"7 ORE => 5 C",
+			"3 A, 4 B => 1 AB",
+			"5 B, 7 C => 1 BC",
+			"4 C, 1 A => 1 CA",
+			"2 AB, 3 BC, 4 CA => 1 FUEL",
+		},
+		165,
+	},
+	{
+		[]string{
+			"157 ORE => 5 NZVS",
+			"165 ORE => 6 DCFZ",
+			"44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL",
+			"12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ",
+			"179 ORE => 7 PSHF",
+			"177 ORE => 5 HKGWZ",
+			"7 DCFZ, 7 PSHF => 2 XJWVT",
+			"165 ORE => 2 GPVTF",
+			"3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT",
+		},
+		13312,
+	},
+	{
+		[]string{
+			"2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG",
+			"17 NVRVD, 3 JNWZP => 8 VPVL",
+			"53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL",
+			"22 VJHF, 37 MNCFX => 5 FWMGM",
+			"139 ORE => 4 NVRVD",
+			"144 ORE => 7 JNWZP",
+			"5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC",
+			"5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV",
+			"145 ORE => 6 MNCFX",
+			"1 NVRVD => 8 CXFTF",
+			"1 VJHF, 6 MNCFX => 4 RFSQX",
+			"176 ORE => 6 VJHF",
+		},
+		180697,
+	},
+}
+
+func TestDetermineRequiredOre(t *testing.T) {
+	for _, testCase := range testCases {
+		ore := DetermineRequiredOre(testCase.reactions)
+		if ore != testCase.expectedOre {
+			t.Errorf("Error, expected %v got %v", testCase.expectedOre, ore)
+		}
+	}
+}
+
 func TestPart1(t *testing.T) {
 	bytes, _ := ioutil.ReadFile("input.txt")
 	expected := "Answer: WRONG"
