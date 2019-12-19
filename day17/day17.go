534d4aefb7314d53990fd4d55c2ccf603fa43c15
David Ayers <davidaayers@gmail.com>
2019-12-18 20:18:48 -0700

    Day 17, Part 2 solution
    I ended up solving the puzzle manually (visually). If I get
    some free time I want to re-visit this, but I want to read
    more about compressional algorithms first.

--- day17/day17.go
+++ day17/day17.go
@@ -61,6 +61,32 @@ type Dir struct {
 	dx, dy int
 }
 
+func (dir Dir) Turn(turn string) Dir {
+	if dir == north {
+		if turn == "R" {
+			return east
+		}
+		return west
+	}
+	if dir == south {
+		if turn == "R" {
+			return west
+		}
+		return east
+	}
+	if dir == east {
+		if turn == "R" {
+			return south
+		}
+		return north
+	}
+	// west
+	if turn == "R" {
+		return north
+	}
+	return south
+}
+
 var north = Dir{dx: 0, dy: -1}
 var south = Dir{dx: 0, dy: 1}
 var east = Dir{dx: 1, dy: 0}
@@ -69,12 +95,16 @@ var west = Dir{dx: -1, dy: 0}
 var dirs = []Dir{north, south, east, west}
 
 func ParseMap(toParse string) ScaffoldMap {
-	mapLines := strings.Split(strings.ReplaceAll(toParse, "\r\n", "\n"), "\n")
+	mapLines := strings.Split(strings.TrimSpace(strings.ReplaceAll(toParse, "\r\n", "\n")), "\n")
 	scaffoldMap := NewScaffoldMap(len(mapLines[0]), len(mapLines))
 
 	for y, line := range mapLines {
 		for x, ch := range line {
 			scaffoldMap.grid[y][x] = Point{x: x, y: y, tile: string(ch)}
+			if string(ch) == "^" {
+				scaffoldMap.startX = x
+				scaffoldMap.startY = y
+			}
 		}
 	}
 	scaffoldMap.RenderMap()
@@ -111,19 +141,148 @@ func Part1(input string) string {
 		str += string(i)
 	}
 
-	alignment := CheckAlignment(ParseMap(str))
+	scaffoldMap := ParseMap(str)
+	alignment := CheckAlignment(scaffoldMap)
 
 	return "Answer: " + strconv.Itoa(alignment)
 }
 
 // Part2 Part2 of puzzle
 func Part2(input string) string {
+	intCode := ParseIntCode(input)
+	memory := make([]int, 5000)
+	copy(memory, intCode)
+
+	output, _, _, _ := RunIntCode(memory, []int{}, 0, 0, false)
+
+	str := ""
+	for _, i := range output {
+		str += string(i)
+	}
+
+	scaffoldMap := ParseMap(str)
+
+	movements := make([]string, 0)
+	facing := north
+	robotX, robotY := scaffoldMap.startX, scaffoldMap.startY
+	turn := "R"
+	steps := 0
+
+	for {
+
+		//fmt.Printf("Steps: %v, Facing: %v, x:%v y: %v\n", steps, facing, robotX, robotY)
+
+		cx := robotX + facing.dx
+		cy := robotY + facing.dy
+
+		if cx < 0 || cx > scaffoldMap.width-1 || cy < 0 || cy > scaffoldMap.height-1 ||
+			scaffoldMap.grid[cy][cx].tile == "." || scaffoldMap.grid[cy][cx].tile == "" {
+			if steps != 0 {
+				movements = append(movements, turn)
+				movements = append(movements, strconv.Itoa(steps))
+			}
+			steps = 0
+
+			var ok bool
+			facing, turn, ok = determineTurnDirection(scaffoldMap, facing, robotX, robotY)
+
+			// we we couldn't turn, we hit the wall, and we're done
+			if !ok {
+				break
+			}
+		} else if scaffoldMap.grid[cy][cx].tile == "#" {
+			robotX += facing.dx
+			robotY += facing.dy
+			steps++
+		}
+	}
+
+	fmt.Printf("%v: %v\n", movements, len(movements)/2)
+
+	memory2 := make([]int, 5000)
+	copy(memory2, intCode)
+	memory2[0] = 2
+
+	intCodeInput := []int{}
+
+	// used this regex (found on reditt):
+	// ^(.{1,20})\1*(.{1,20})(?:\1|\2)*(.{1,20})(?:\1|\2|\3)*$
+	// to solve manually; I'll come back and try to do this programatically.
+
+	// R,8,L,10,R,8,R,12,R,8,L,8,L,12,R,8,L,10,R,8,L,12,L,10,L,8,R,8,L,10,R,8,R,12,R,8,L,8,L,12,L,12,L,10,L,8,L,12,L,10,L,8,R,8,L,10,R,8,R,12,R,8,L,8,L,12
+	order := "A,B,A,C,A,B,C,C,A,B\n"
+	a := "R,8,L,10,R,8\n"
+	b := "R,12,R,8,L,8,L,12\n"
+	c := "L,12,L,10,L,8\n"
+
+	intCodeInput = append(intCodeInput, toIntSlice(order)...)
+	fmt.Printf("%v\n", intCodeInput)
+	intCodeInput = append(intCodeInput, toIntSlice(a)...)
+	fmt.Printf("%v\n", intCodeInput)
+	intCodeInput = append(intCodeInput, toIntSlice(b)...)
+	fmt.Printf("%v\n", intCodeInput)
+	intCodeInput = append(intCodeInput, toIntSlice(c)...)
+	fmt.Printf("%v\n", intCodeInput)
+
+	// add the interactive flag
+	intCodeInput = append(intCodeInput, toIntSlice("n\n")...)
+
+	fmt.Printf("%v\n", intCodeInput)
+
+	output2, _, _, _ := RunIntCode(memory2, intCodeInput, 0, 0, false)
+
+	fmt.Printf("%v\n", output2)
+
+	str = ""
+	for _, i := range output2 {
+		if i > 46 {
+			fmt.Printf("***")
+		}
+		r := string(rune(i))
+		fmt.Printf("%v", r)
+		str += string(i)
+	}
+
+	fmt.Println(str)
+
+	scaffoldMap = ParseMap(str)
+	scaffoldMap.RenderMap()
+
 	return "Answer: "
+}
+
+func toIntSlice(str string) []int {
+	sl := make([]int, 0)
+	for _, r := range str {
+		sl = append(sl, int(r))
+	}
+	return sl
+}
+
+func determineTurnDirection(scaffoldMap ScaffoldMap, facing Dir, x int, y int) (Dir, string, bool) {
+	// now figure out if it's a right or left turn
+	tryTurn := []string{"R", "L"}
+
+	var checkTurn Dir
+	for _, t := range tryTurn {
+		tryDir := facing.Turn(t)
+
+		tx := x + tryDir.dx
+		ty := y + tryDir.dy
+
+		if tx < 0 || tx > scaffoldMap.width-1 || ty < 0 || ty > scaffoldMap.height-1 ||
+			scaffoldMap.grid[ty][tx].tile != "#" {
+			continue
+		}
+
+		return tryDir, t, true
+	}
+	return checkTurn, "", false
 }
 
 func main() {
 	bytes, _ := ioutil.ReadFile("input.txt")
 
-	fmt.Println("Part 1: " + Part1(string(bytes)))
+	//fmt.Println("Part 1: " + Part1(string(bytes)))
 	fmt.Println("Part 2: " + Part2(string(bytes)))
 }
